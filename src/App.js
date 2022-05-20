4c1f899a1bc8e3180ba3d93b69807d1e97dcab95
Junaid Bukhari <imjunaid1997@gmail.com>
2022-05-20 16:35:48 +0500

    login page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './HOC/footer';
 import Header from './HOC/header';
 import Start from './pages/start';
 import Menu from './pages/menu';
+import Auth from './pages/auth';
 import NotFound from './pages/notFound';
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Header />
         <Routes>
           <Route path='/' element={<Start />}></Route>
+          <Route path='/login' element={<Auth />}></Route>
           <Route path='/menu' element={<Menu />}></Route>
           <Route path='/*' element={<NotFound />}></Route>
         </Routes>
