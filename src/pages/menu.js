25eead25ceaecf325ca80c301a62c347fa572d04
Junaid Bukhari <imjunaid1997@gmail.com>
2022-05-20 15:33:54 +0500

    menu and responsiveness

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -5,86 +5,68 @@ const Menu = () => {
     {
       id: 1,
       title: 'Grilled Chicken Shawarma',
+      price: 250,
       image:
         'https://hips.hearstapps.com/vidthumb/images/190130-chicken-shwarma-horizontal-1551285400.png',
     },
     {
       id: 2,
       title: 'Pizza',
+      price: 450,
       image:
         'https://mahatmarice.com/wp-content/uploads/2020/04/Rice-Pizza-Crust.jpg',
     },
     {
       id: 3,
       title: 'Zinger Burger',
+      price: 200,
       image:
         'https://mir-s3-cdn-cf.behance.net/projects/original/75cabb95034859.Y3JvcCw3NTgsNTkzLDI3NCwxNTU.jpg',
     },
     {
       id: 4,
       title: 'Chicken Biryani',
+      price: 200,
       image: 'https://hamariweb.com/recipes/images/recipeimages/3.jpg',
     },
     {
       id: 5,
       title: 'Tandoori Chicken Wrap',
+      price: 350,
       image:
         'https://static.toiimg.com/thumb/62194857.cms?width=1200&height=900',
     },
     {
       id: 6,
       title: 'Grilled Fish',
-      image: 'https://hamariweb.com/recipes/images/recipeimages/70962.jpg',
-    },
-    {
-      id: 7,
-      title: 'Grilled Chicken Shawarma',
-      image:
-        'https://hips.hearstapps.com/vidthumb/images/190130-chicken-shwarma-horizontal-1551285400.png',
-    },
-    {
-      id: 8,
-      title: 'Pizza',
-      image:
-        'https://mahatmarice.com/wp-content/uploads/2020/04/Rice-Pizza-Crust.jpg',
-    },
-    {
-      id: 9,
-      title: 'Zinger Burger',
-      image:
-        'https://mir-s3-cdn-cf.behance.net/projects/original/75cabb95034859.Y3JvcCw3NTgsNTkzLDI3NCwxNTU.jpg',
-    },
-    {
-      id: 10,
-      title: 'Chicken Biryani',
-      image: 'https://hamariweb.com/recipes/images/recipeimages/3.jpg',
-    },
-    {
-      id: 11,
-      title: 'Tandoori Chicken Wrap',
-      image:
-        'https://static.toiimg.com/thumb/62194857.cms?width=1200&height=900',
-    },
-    {
-      id: 12,
-      title: 'Grilled Fish',
+      price: 550,
       image: 'https://hamariweb.com/recipes/images/recipeimages/70962.jpg',
     },
   ];
   return (
-    <div className='container'>
-      <div style={{ marginBottom: '100px' }} className='row card-group'>
+    <div className='container d-flex justify-content-center'>
+      <div style={{ marginBottom: '100px' }} className='row '>
         {data.map((d) => (
-          <div key={d.id} className='col col-4 mt-4'>
+          <div
+            style={{ maxWidth: '350px', minWidth: '350px' }}
+            key={d.id}
+            className='col col-lg-6 col-xl-4 col-md-12 mt-4'
+          >
             <img
               className='card-img-top'
-              height='200px'
+              height={230}
               src={d.image}
               alt='Card image cap'
             />
             <div style={{ backgroundColor: 'white' }} className='card-body'>
-              <h5 className='card-title'>{d.title}</h5>
-              <button className='btn btn-success' onclick={() => {}}>
+              <span
+                className='d-flex'
+                style={{ justifyContent: 'space-between' }}
+              >
+                <h5>{d.title}</h5>
+                <h5 style={{ color: 'green' }}>Rs.{d.price}</h5>
+              </span>
+              <button className='btn btn-success' onClick={() => {}}>
                 ADD TO CART
               </button>
             </div>
